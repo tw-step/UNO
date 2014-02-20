601ba400a155c38ce30fef58b73e4da54a56b02a
prajakta <prajaktv@thoghtworks.com>
2014-02-20 18:34:50 +0530

    #3002:Bugs are fixed.

--- src/com/step/uno/client/screen/PlayerScreen.java
+++ src/com/step/uno/client/screen/PlayerScreen.java
@@ -45,5 +45,6 @@ public class PlayerScreen extends JFrame implements PlayerView {
     public void update(Snapshot snapshot) {
         closePile.update(snapshot);
         openPile.update(snapshot);
+
     }
 }
