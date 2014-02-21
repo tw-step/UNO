638a6423a3aef36a0eff7f0091d0fb15431b8136
Kajal <kajal@inankhome.com>
2014-02-21 18:11:43 +0530

    #3005: current player name added to player screen

--- src/com/step/uno/client/screen/PlayerScreen.java
+++ src/com/step/uno/client/screen/PlayerScreen.java
@@ -17,6 +17,8 @@ public class PlayerScreen extends JFrame implements PlayerView {
     private PlayersView playersView;
     private ActivityLog activityLog;
     private MyCards myCards;
+    private final JPanel currentPlayer;
+    private final JLabel player;
 
     public PlayerScreen(GameClient gameClient, Snapshot snapshot) {
         this.gameClient = gameClient;
@@ -27,14 +29,20 @@ public class PlayerScreen extends JFrame implements PlayerView {
         myCards = new MyCards(this.gameClient,this.snapshot);
         openPile = new OpenPile(this.snapshot);
         activityLog =new ActivityLog();
+        currentPlayer = new JPanel();
+        player = new JLabel(snapshot.playerSummaries[snapshot.currentPlayerIndex].name+"\'s turn");
+        player.setFont(new Font("vardana",Font.BOLD,25));
 
         openPile.setBounds(900, 400, 250, 300);
         closePile.setBounds(600, 400, 250, 300);
         playersView.setBounds(40, 0, 1200, 300);
         myCards.setBounds(300, 0, 900, 200);
-
+        currentPlayer.setBounds(600,200,300,200);
         setLayout(new BorderLayout());
-
+        currentPlayer.add(player);
+        currentPlayer.setPreferredSize(new Dimension(200, 200));
+        currentPlayer.setVisible(true);
+        add(currentPlayer);
         add(openPile);
         add(closePile);
         add(playersView);
@@ -58,5 +66,7 @@ public class PlayerScreen extends JFrame implements PlayerView {
         playersView.update(snapshot);
         activityLog.update(snapshot);
         myCards.update(snapshot);
+        player.setText(snapshot.playerSummaries[snapshot.currentPlayerIndex].name+"\'s turn");
+
     }
 }
